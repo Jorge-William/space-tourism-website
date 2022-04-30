e51a7212543c96d6e94e0530b50187bb1b8ffc78
Jorge William <jorge_william_cardoso@yahoo.com.br>
2022-04-30 14:52:12 -0300

    Feat: Nav bar com itens estáticos

--- src/Components/navbar/Navbar.js
+++ src/Components/navbar/Navbar.js
@@ -1,28 +1,45 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './Navbar.css'
+import logo from '../../Assets/shared/logo.svg'
 
 class Navbar extends React.Component {
 	render() {
 		return (
-			<section>
-				<nav>
+			<div id='container'>
+				<div id='logo'>
+					<img src={logo} alt='logotype' />
+				</div>
+				<hr />
+				<nav id='nav-buttons-container'>
 					<ul>
 						<li>
-							<Link to='/'>00 Home</Link>
+							<Link to='/'>
+								<span className='numeracao-menu'>00</span>
+								HOME
+							</Link>
 						</li>
 						<li>
-							<Link to='/destination'>01 Destination</Link>
+							<Link to='/destination'>
+								<span className='numeracao-menu'>01</span>
+								DESTINATION
+							</Link>
 						</li>
 						<li>
-							<Link to='/crew'>02 Crew</Link>
+							<Link to='/crew'>
+								<span className='numeracao-menu'>02</span>
+								CREW
+							</Link>
 						</li>
 						<li>
-							<Link to='/technology'>03 Technology</Link>
+							<Link to='/technology'>
+								<span className='numeracao-menu'>03</span>
+								TECHNOLOGY
+							</Link>
 						</li>
 					</ul>
 				</nav>
-			</section>
+			</div>
 		)
 	}
 }
