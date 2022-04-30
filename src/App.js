1d594c30a7a4a367b47023e75c4998131d5ae36c
Jorge William <jorge_william_cardoso@yahoo.com.br>
2022-04-30 12:07:01 -0300

    Fix: Remodelando esquema de paginas

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 // import './App.css'
 import React from 'react'
-import Navbar from './Components/Navbar'
+import HomePage from './Pages/Home.page'
 
 function App() {
 	return (
 		<div>
-			<Navbar />
-			<p>
-				So, you want to travel to Space Let’s face it; if you want to go
-				to space, you might as well genuinely go to outer space and not
-				hover kind of on the edge of it. Well sit back, and relax
-				because we’ll give you a truly out of this world experience!
-				Explore
-			</p>
+			<HomePage />
 		</div>
 	)
 }
--- src/Pages/Home.page.js
+++ src/Pages/Home.page.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import Navbar from '../Components/navbar/Navbar'
 
 class Home extends React.Component {
 	render() {
-		return {}
+		return (
+			<div>
+				<Navbar />
+				<p>
+					So, you want to travel to Space Let’s face it; if you want
+					to go to space, you might as well genuinely go to outer
+					space and not hover kind of on the edge of it. Well sit
+					back, and relax because we’ll give you a truly out of this
+					world experience! Explore
+				</p>
+			</div>
+		)
 	}
 }
 
